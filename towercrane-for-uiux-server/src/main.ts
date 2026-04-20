bcabe4e9009c2a8cf7ea5388db702a55cbba5fbe
oh hyun seok <hyunsokstar@gmail.com>
2026-04-20 17:18:24 +0900

    Fix: CORS for hibot-docu.com and simplify detail dialog for build test

--- towercrane-for-uiux-server/src/main.ts
+++ towercrane-for-uiux-server/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+    origin: [
+      'http://localhost:5174',
+      'http://127.0.0.1:5174',
+      'https://hibot-docu.com',
+      'http://hibot-docu.com',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
